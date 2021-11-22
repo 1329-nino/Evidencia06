7330673432539298e622592464b68dae44b736b3
ninodl <n.diaz10@ufromail.cl>
2021-11-22 18:11:05 -0300

    Casos creados

---
+++ src/main/java/Cuaderno_Universitario/Cuaderno.java
@@ -0,0 +1,22 @@
+package Cuaderno_Universitario;
+
+public class Cuaderno {
+	private int cantHojas;
+	public Hoja _..100;
+
+	public void escribir() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void arrancarHoja() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int getCantHojas() {
+		return this.cantHojas;
+	}
+
+	public void setCantHojas(int aCantHojas) {
+		this.cantHojas = aCantHojas;
+	}
+}
---
+++ src/main/java/Estimar_Persona/Persona.java
@@ -0,0 +1,12 @@
+package Estimar_Persona;
+
+public class Persona {
+
+	public void estimar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void considerarAmigo() {
+		throw new UnsupportedOperationException();
+	}
+}
